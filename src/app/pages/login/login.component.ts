02f5111f0d8b47c88210530ee7c576060fb137d2
Juan Pregliasco <juanpipre@outlook.com>
2023-12-04 21:25:11 -0300

    Avances en form registro e inicio de sesion

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  public login : boolean = true;
+
+  public toggleLogin():void{
+    this.login = ! this.login;
+  }
+
+}
