e4c93f3cceef0133db70f49a17f6aa39cc8f2955
Juan Pregliasco <juanpipre@outlook.com>
2023-12-05 15:17:17 -0300

    FOrm inicio de sesion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { Component } from '@angular/core';
+import { DashboardComponent } from './pages/employee/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: '**', redirectTo: 'login' },
-    {path:'login',component:LoginComponent}
+    
+    {path:'login',component:LoginComponent,},
+    {path: 'employee',component:DashboardComponent,children:[
+        
+    ]},
+    { path: '**', redirectTo: 'login', pathMatch:'full' }
 ];
