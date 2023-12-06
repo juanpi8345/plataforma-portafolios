b8b9bb3bcd47df7d1010a2616dde5d82d8d41795
Juan Pregliasco <juanpipre@outlook.com>
2023-12-06 19:36:24 -0300

    Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
   public login : boolean = true;
+  public isVisible : boolean = false; 
 
   public toggleLogin():void{
     this.login = ! this.login;
+  }
+
+  onElementVisible(isVisible: boolean) {
+    this.isVisible = isVisible;
   }
 
 }
