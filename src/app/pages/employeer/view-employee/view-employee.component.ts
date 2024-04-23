378531d6535eca0b058526099ba44ce078743342
juanpi8345 <juanpipre@outlook.com>
2024-04-23 13:58:01 -0300

    view employee

--- src/app/pages/employeer/view-employee/view-employee.component.ts
+++ src/app/pages/employeer/view-employee/view-employee.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { EmployeeService } from '../../../services/employee.service';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map } from 'rxjs';
 import { EmployerService } from '../../../services/employer.service';
 import { CommonModule } from '@angular/common';
 import { ProfileService } from '../../../services/profile.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-view-employee',
@@ -16,7 +17,8 @@ import { ProfileService } from '../../../services/profile.service';
 export class ViewEmployeeComponent {
 
   constructor(private router: Router , private employerService:EmployerService,
-    private route:ActivatedRoute, private profileService:ProfileService) {}
+    private route:ActivatedRoute, private profileService:ProfileService,
+    private authService:AuthService, private employeeService:EmployeeService) {}
 
   profileId : number;
   employee$!: Observable<any>;
@@ -24,7 +26,20 @@ export class ViewEmployeeComponent {
   ngOnInit() {
     this.route.params.subscribe(params=>{
       this.profileId = this.route.snapshot.params['profileId'];
-      this.employee$ = this.employerService.getEmployee(this.profileId);
+      this.employee$ = this.employerService.getEmployee(this.profileId).pipe(catchError(error => {
+        this.authService.logout();
+        this.router.navigate(['/login']);
+        throw new Error;
+        //Hay que agregar las imagenes de los proyectos
+        //El get de profile seria muy grande si tambien recibe las imagenes
+      }),map((emplyoee: any) => {
+        emplyoee.projects.forEach(project => {
+          this.employeeService.getProjectImage(project.projectId).subscribe(imageUrl => {
+            project.image = imageUrl;
+          });
+        });
+        return emplyoee;
+      }));;
       this.loadImage()
     })
 
